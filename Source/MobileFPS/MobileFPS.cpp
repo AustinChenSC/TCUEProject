4ae7c21d7fb2a54bc1a1b877972d38be8a242e1a
AustinChenSC <972686420@qq.com>
2023-09-18 12:22:31 +0800

    第一次提交

---
+++ Source/MobileFPS/MobileFPS.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MobileFPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MobileFPS, "MobileFPS" );
+ 
---
+++ Source/MobileFPS/MobileFPSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MobileFPSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMobileFPSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMobileFPSGameMode();
+};
+
+
+
